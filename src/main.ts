7cb3cc51321388184c4cfdb38ff6c5bb393023ee
storious <2227563488@qq.com>
2024-07-25 23:12:44 +0800

    import tailwind

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import ArcoVue from '@arco-design/web-vue';
 import App from './App.vue';
 import '@icon-park/vue-next/styles/index.css';
 import '@arco-design/web-vue/dist/arco.css';
+import "tailwindcss/tailwind.css"
 import router from './router';
 import store from './store';
 import "./access"
